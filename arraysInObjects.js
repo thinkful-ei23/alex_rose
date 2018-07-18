9f9467b6eceaf9f9727cecaa45f42e68b517ad55
Alex Puhl <apuhlswimmer@hotmail.com>
2018-07-18 14:06:32 -0600

    arraysInObjects

---
+++ arraysInObjects.js
@@ -0,0 +1,14 @@
+'use strict';
+const obj = {
+  meals : [
+    'breakfast',
+    'second breakfast',
+    'elevenses',
+    'lunch',
+    'afternoon tea',
+    'dinner',
+    'supper'
+  ]
+};
+
+console.log(obj.meals[3]);
